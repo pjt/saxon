7429eadaae903b24e9d0f9b50c1e157c781a2d8d
Perry Trolard <trolard@gmail.com>
2009-12-03 16:48:58 -0600

    Changed to leiningen for dependency mgmt & build; no more jars stored in git repo! Clojars now holds the Saxon dependencies.
    Also added main entrypoint to saxon ns, which duplicates what bin/xquery
    does.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clojure-saxon "0.1"
+            :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
+            :main saxon
+            :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                           [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                           [org.clojars.pjt/saxon9 "9.1.0.8"]
+                           [org.clojars.pjt/saxon9-s9api "9.1.0.8"]])
