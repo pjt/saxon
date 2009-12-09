fdb43aa707391a3889c6d3690b5e3ef6c97e31f5
Perry Trolard <trolard@gmail.com>
2009-12-09 16:19:18 -0600

    Using resources directory for license, notices.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clojure-saxon "0.1"
+(defproject clojure-saxon "0.9.0-SNAPSHOT"
             :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
             :main saxon
-            :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+            :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                            [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                            [org.clojars.pjt/saxon9 "9.1.0.8"]
                            [org.clojars.pjt/saxon9-s9api "9.1.0.8"]])
