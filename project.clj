0132c7e70c521e26bb2099712e6cffe68c58e591
Perry Trolard <trolard@gmail.com>
2011-10-18 11:59:15 -0500

    Depend on Clojure 1.3 release.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-saxon "0.9.2"
             :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
             :main saxon
-            :dependencies [[org.clojure/clojure "1.3.0-beta3"]
+            :dependencies [[org.clojure/clojure "1.3.0"]
                            [net.sourceforge.saxon/saxon "he9.3.0.5j"]])
