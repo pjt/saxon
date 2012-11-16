89b5d3549d99e49c5c15196fb7f8f93cc49f9f21
Perry Trolard <trolard@gmail.com>
2012-11-16 13:27:44 -0600

    Disable AOT.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject clojure-saxon "0.9.2"
             :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
-            :main saxon
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.clojars.pjt/saxon9 "9.1.0.8"]
                            [org.clojars.pjt/saxon9-s9api "9.1.0.8"]])
