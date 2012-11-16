20fadc2b5ed57ccf550990b7cc6964c41f36d4e5
Perry Trolard <trolard@gmail.com>
2012-11-16 15:52:03 -0600

    Bump to version 0.9.3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-saxon "0.9.2"
+(defproject clojure-saxon "0.9.3"
             :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.clojars.pjt/saxon9 "9.1.0.8"]
